b42e9049350712fd04318864da2ee87917ea0409
Kevin Yu <kevinyu@Kevins-MacBook-Pro.local>
2020-05-13 12:02:12 -0400

    updated

---
+++ docs/precache-manifest.4f7cb0cd473351adcb9cac38bc20dca2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "559698f2f36a42f0cf38ce3db9d65dd2",
+    "url": "/prop-it-up/index.html"
+  },
+  {
+    "revision": "b60e5b97c5d5f90021d8",
+    "url": "/prop-it-up/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "218d5a9ae1cf15414ca8",
+    "url": "/prop-it-up/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b60e5b97c5d5f90021d8",
+    "url": "/prop-it-up/static/js/2.4eb195d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prop-it-up/static/js/2.4eb195d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "218d5a9ae1cf15414ca8",
+    "url": "/prop-it-up/static/js/main.7ba167d8.chunk.js"
+  },
+  {
+    "revision": "d4f060ba53f68fed2a76",
+    "url": "/prop-it-up/static/js/runtime-main.175975c5.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ca6172b051091fe333a836bb0fdc5fef.js"
+  "/prop-it-up/precache-manifest.4f7cb0cd473351adcb9cac38bc20dca2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/prop-it-up/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
